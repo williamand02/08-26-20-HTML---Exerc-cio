8daacc672a27aea2840d567ff39238089b568dd8
william <williamstrayder@gmail.com>
2020-08-27 03:09:41 -0300

    Signed-off-by: william <williamstrayder@gmail.com> Primeira versão

---
+++ js/global.js
@@ -0,0 +1,274 @@
+
+///json
+var cadeiras = [
+    {
+        "foto": "/medias/sys_master/images/images/hbe/h12/9578857693214/0409680-01.jpg",
+        "nome": "Cadeira Shell de Madeira e Aço Carbono Preta"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hbe/h12/9578857693214/0409680-01.jpg",
+        "nome": "Cadeira Shell de Madeira e Aço Carbono Preta"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h45/hb8/9499191312414/0392886-01.jpg",
+        "nome": "Cadeira Aproximação Umbra Oh Chair de Polipropileno Preta"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hc1/h20/9652597293086/0393619-01.jpg",
+        "nome": "Cadeira com Braço Penha de Polipropileno Marrom"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hed/hda/9652600111134/0393620-01.jpg",
+        "nome": "Cadeira Penha de Polipropileno Cinza"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h1f/h72/9652603387934/0393622-01.jpg",
+        "nome": "Cadeira Penha de Polipropileno Marrom"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hd9/hec/9544340930590/0409363-01.jpg",
+        "nome": "Cadeira com Braço Léo de Aço Carbono Niquelado"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h29/h8e/9544417083422/0409487-01.jpg",
+        "nome": "Cadeira Matilde de Madeira Natural e Branca"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h6b/h15/9544412495902/0409478-01.jpg",
+        "nome": "Cadeira com Braço Arles de Madeira Natural e Branca"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h83/h93/9513723330590/0409473-01.jpg",
+        "nome": "Cadeira Pollin de Madeira Maciça Amarela"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h7c/h25/9660227878942/0409947-01.jpg",
+        "nome": "Cadeira Campestre II de Madeira Maciça Avelã"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h78/hec/9544661041182/0409946-01.jpg",
+        "nome": "Cadeira com Braço Campestre II de Madeira Maciça Avelã"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hff/h25/9544584855582/0409760-01.jpg",
+        "nome": "Cadeira Aproximação Will de PVC e Couro Natural Preta"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hff/h25/9544584855582/0409760-01.jpg",
+        "nome": "Cadeira Aproximação Will de PVC e Couro Natural Preta"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hb8/hb7/9627479670814/0409595-01.jpg",
+        "nome": "Cadeira Eames III de Madeira e Fibra Sintética Preta"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/ha2/h28/9544521023518/0409685-01.jpg",
+        "nome": "Cadeira Secretária Office Shell de Aço Cromado Cinza"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hdc/h35/9514007560222/0409670-01.jpg",
+        "nome": "Cadeira Sheel Madeira Natural e Amarela"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h69/h95/9627521548318/0410625-01.jpg",
+        "nome": "Cadeira Infantil Eames de Abs Natural e Azul Claro"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hbd/h93/9652832337950/0411053-01.jpg",
+        "nome": "Cadeira Macela de Polipropileno Amarela"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h16/h59/9627555299358/0411054-01.jpg",
+        "nome": "Cadeira Macela de Polipropileno Marsala"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hc2/h74/9545105506334/0411048-01.jpg",
+        "nome": "Cadeira com Braço Macela de Polipropileno Branca"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hd2/h43/9618453266462/0411180-01.jpg",
+        "nome": "Cadeira Vinca II de Madeira Maciça e Poliuretano Caramelo"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h19/heb/9618438684702/0411176-01.jpg",
+        "nome": "Cadeira Hoya II de Madeira Maciça e Poliuretano Preta"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h4f/h4f/9618444910622/0411177-01.jpg",
+        "nome": "Cadeira Nicole II de Madeira Maciça e Poliuretano Branca"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h4f/h4f/9618444910622/0411177-01.jpg",
+        "nome": "Cadeira Nicole II de Madeira Maciça e Poliuretano Branca"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hc1/hea/9545414443038/0411960-01.jpg",
+        "nome": "Cadeira Diretor Will de PVC e Couro Natural Preta"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hc1/hea/9545414443038/0411960-01.jpg",
+        "nome": "Cadeira Diretor Will de PVC e Couro Natural Preta"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h7c/h5b/9545505210398/0412159-01.jpg",
+        "nome": "Cadeira Aproximação com Braço Skin II de Couro Natural Caramelo e Preta"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h7c/h5b/9545505210398/0412159-01.jpg",
+        "nome": "Cadeira Aproximação com Braço Skin II de Couro Natural Caramelo e Preta"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hf6/h8d/9578975920158/0412162-01.jpg",
+        "nome": "Cadeira Aproximação Skin II de Couro Natural Caramelo"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hf6/h8d/9578975920158/0412162-01.jpg",
+        "nome": "Cadeira Aproximação Skin II de Couro Natural Caramelo"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h80/hcb/9627605631006/0412743-01.jpg",
+        "nome": "Cadeira Easy de Polipropileno Azul"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hf6/h52/9652839546910/0413312-01.jpg",
+        "nome": "Cadeira Potim II de Polipropileno Branca e Cromada"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hde/h1c/9565040115742/0414330-01.jpg",
+        "nome": "Cadeira Infantil Wishbone Branca"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hc5/h52/9581003014174/0414329-01.jpg",
+        "nome": "Cadeira Infantil Wishbone Bege"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h13/hb4/9519126642718/0414340-01.jpg",
+        "nome": "Cadeira Infantil Boppard de Madeira Natural"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h69/hd0/9636906532894/0414238-01.jpg",
+        "nome": "Cadeira Luigi de Plástico Branca"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h69/hd0/9636906532894/0414238-01.jpg",
+        "nome": "Cadeira Luigi de Plástico Branca"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h4b/h53/9565042147358/0414365-01.jpg",
+        "nome": "Cadeira Infantil Luigi II de Madeira"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h28/hb0/9565045424158/0414366-01.jpg",
+        "nome": "Cadeira Infantil Luigi II de Madeira Branca"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h2c/hc9/9519129002014/0414341-01.jpg",
+        "nome": "Cadeira Infantil Boppard de Madeira Branca"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h87/h45/9519134474270/0414350-01.jpg",
+        "nome": "Cadeira Luigi II de Madeira Maciça"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h47/h3f/9546155655198/0414711-01.jpg",
+        "nome": "Cadeira de Polipropileno Kamon Cinza"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hd5/hee/9519550988318/0414723-01.jpg",
+        "nome": "Cadeira Zirav de Polipropileno Preta"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h6f/hb3/9581007339550/0414708-01.jpg",
+        "nome": "Cadeira Boppard de Polipropileno Branca"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hd5/h1a/9546275815454/0415808-01.jpg",
+        "nome": "Cadeira Lena de Madeira Maciça e Polipropileno Marrom"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h99/hcc/9652923727902/0416374-01.jpg",
+        "nome": "Cadeira Laila II de Madeira Maciça Cinza Claro e Ébano"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h42/hd7/9546879827998/0421165-01.jpg",
+        "nome": "Cadeira Marcele de Madeira Maciça e Algodão Areia e Imbuia"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h29/h1e/9660342894622/0421176-01.jpg",
+        "nome": "Cadeira com Braço Claire de PVC Preta e Ébano"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hb9/h5b/9660340011038/0421174-01.jpg",
+        "nome": "Cadeira com Braço Claire Palha de Madeira Natural e Trama Cru e Imbuia"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hb9/h5b/9660340011038/0421174-01.jpg",
+        "nome": "Cadeira com Braço Claire Palha de Madeira Natural e Trama Cru e Imbuia"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h86/h5f/9660347613214/0421181-01.jpg",
+        "nome": "Cadeira Slope II de Madeira Maciça e PVC Areia e Imbuia"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h86/h5f/9660347613214/0421181-01.jpg",
+        "nome": "Cadeira Slope II de Madeira Maciça e PVC Areia e Imbuia"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h82/h25/9525376548894/0422059-01.jpg",
+        "nome": "Espreguiçadeira Arpoador de Madeira Natural"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hac/h60/9674487922718/0422071-01.jpg",
+        "nome": "Cadeira Arpoador de Madeira Natural"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/hd6/h04/9547028299806/0422072-01.jpg",
+        "nome": "Cadeira com Braço Arpoador de Madeira Natural"
+    },
+    {
+        "foto": "/medias/sys_master/images/images/h35/h68/9652998799390/0422170-01.jpg",
+        "nome": "Cadeira Théo II de Madeira Maciça e PVC Ébano e Preta"
+    }
+]
+window.onload = initPage;
+
+function initPage() {
+
+    //Nome do site onde peguei o link das imagens e os nomes
+    var etna = "https://www.etna.com.br/"
+    cadeiras.forEach(cadeira => {
+        let tr = document.createElement("tr");
+        Object.keys(cadeira).forEach(function (item) {
+            if (item == "foto") {
+                let img = document.createElement("img");
+                img.src = etna + cadeira[item];
+                let td = document.createElement("td");
+                td.append(img);
+                tr.append(td);
+                return
+            }
+            let p = document.createElement("p");
+            p.textContent = cadeira[item]
+            let td = document.createElement("td");
+                td.append(p);
+            tr.append(td);
+        });
+        tr.addEventListener('click', modalProduto);
+        document.querySelector("tbody").appendChild(tr)
+    });
+    function modalProduto(e) {
+        let display = document.querySelector(".modal-produto").style.display;
+        document.querySelector(".modal-produto").style.display = display == ''? 'block':'';
+        if(document.querySelector(".modal-produto").style.display == 'block'){
+            let corpo = document.querySelector('.corpo-modal');
+                corpo.innerHTML="";
+                corpo.appendChild(this.querySelector('img'))
+                corpo.appendChild(this.querySelector('p'))
+        }
+        // document.querySelector(".modal-produto")
+        console.log(this)
+        console.log("clicou")
+    }
+    document.querySelector(".modal-produto").addEventListener('click', modalProduto);
+
+}
